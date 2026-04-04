c6309140e7dece3aa1f0e0a2452a601c7ac900cf
Dreacel <143748787+Dreacel@users.noreply.github.com>
2026-04-03 19:19:54 -0500

    Update site.js

--- site.js
+++ site.js
@@ -5,5 +5,13 @@ const plantflower = document.querySelector('.plant')
 plantsoil.addEventListener('click', () => {
     if(plantflower.classList.contains('plant')) {
         plantflower.classList.add('active');
+        plantsoil.classList.remove('active');
+    }
+});
+
+plantflower.addEventListener('click', () => {
+    if(plantsoil.classList.contains('soil')) {
+        plantsoil.classList.add('active');
+        plantflower.classList.remove('active');
     }
 });
